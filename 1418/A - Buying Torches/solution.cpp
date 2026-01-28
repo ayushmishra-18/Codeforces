d53becd624f26428f9d33fab23db3995856e1255
Ayush Mishra <2006ayushmishra@gmail.com>
2026-01-28 20:22:15 +0530

    Add Buying Torches [A] from Codeforces

---
+++ 1418/A - Buying Torches/solution.cpp
@@ -0,0 +1,1 @@
+//A. Buying Torches #include<bits/stdc++.h>using namespace std;#define int long long intsigned main(){    int t;    cin>>t;    while(t--){        int x,y,k;        cin>>x>>y>>k;        int a=k*y+k-1;        int b=((a+x-2)/(x-1))+k;        cout<<b<<endl;    }}
