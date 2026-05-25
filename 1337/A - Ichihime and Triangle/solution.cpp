7ec236b20bb137f102aa1557d84627b3cb3fe9f4
Ayush Mishra <2006ayushmishra@gmail.com>
2026-05-25 18:42:34 +0530

    Add Ichihime and Triangle [A] from Codeforces

---
+++ 1337/A - Ichihime and Triangle/solution.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long int
+ 
+void fast_io() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(NULL);
+    std::cout.tie(NULL);
+}
+ 
+signed main() {
+    fast_io();
+    int t;
+    cin>>t;
+    while(t--){
+        int a,b,c,d;
+        cin>>a>>b>>c>>d;
+        cout<<b<<" "<<c<<" "<<c<<" "<<endl;
+    }
+    return 0;
+}
