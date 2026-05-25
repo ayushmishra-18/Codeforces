acc249c8b2214fc58d6465252eefd54d15f81f2c
Ayush Mishra <2006ayushmishra@gmail.com>
2026-05-25 18:28:14 +0530

    Add Beautiful Matrix [A] from Codeforces

---
+++ 263/A - Beautiful Matrix/solution.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long int
+ 
+void fast_io() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(NULL);
+    std::cout.tie(NULL);
+}
+ 
+signed main() {
+    fast_io();
+    int arr[5][5];
+    int a=0,b=0;
+    for(int i=0;i<5;i++){
+        for(int j=0;j<5;j++){
+            cin>>arr[i][j];
+            if(arr[i][j]==1){
+                a=i;
+                b=j;
+            }
+        }
+    }
+    cout<<abs(2-a)+abs(2-b);
+    return 0;
+}
