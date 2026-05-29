67fea73dcfb748f4a75819036d0bef92c7cd3edc
Ayush Mishra <2006ayushmishra@gmail.com>
2026-05-29 16:36:29 +0530

    Add Domino piling [A] from Codeforces

---
+++ 50/A - Domino piling/solution.cpp
@@ -0,0 +1,20 @@
+//A. Domino piling
+ 
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long int
+ 
+void fast_io() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(NULL);
+    std::cout.tie(NULL);
+}
+ 
+signed main() {
+    fast_io();
+    int m,n;
+    cin>>m>>n;
+    cout<<(m*n)/2<<endl;        
+    
+    return 0;
+}
