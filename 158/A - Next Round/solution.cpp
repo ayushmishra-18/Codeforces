5758f1d985b63beb4387c24c4d6727246ebace29
Ayush Mishra <2006ayushmishra@gmail.com>
2026-06-01 15:20:14 +0530

    Add Next Round [A] from Codeforces

---
+++ 158/A - Next Round/solution.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n,k;
+    cin>>n>>k;
+    int pass=0;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    for(int i=0;i<n;i++){
+        if(arr[i]>=arr[k-1] && arr[i]>0){
+            pass++;
+        }
+    }
+    cout<<pass;
+}
