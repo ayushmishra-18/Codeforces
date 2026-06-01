dc537b20543d951e2e6c54ec05c850ee9a045544
Ayush Mishra <2006ayushmishra@gmail.com>
2026-06-01 15:23:24 +0530

    Add Bear and Big Brother [A] from Codeforces

---
+++ 791/A - Bear and Big Brother/solution.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int a,b;
+    cin>>a>>b;
+    for(int i=1;i<1000;i++){
+        a=a*3;
+        b=b*2;
+        if(a>b){
+            cout<<i;
+            return 0;
+        }
+    }
+}
