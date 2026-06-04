eaca1bf1890e85c843b7a4c2a8bad172b42819c7
Ayush Mishra <2006ayushmishra@gmail.com>
2026-06-04 18:52:21 +0530

    Add Twin Permutations [A] from Codeforces

---
+++ 1831/A - Twin Permutations/solution.cpp
@@ -0,0 +1,25 @@
+//A. Twin Permutations
+ 
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        int arr[n];
+        for(int i=0;i<n;i++){
+            cin>>arr[i];
+        }
+        int b[n];
+ 
+        for(int i=0;i<n;i++){
+            b[i]=n-arr[i]+1;
+        }        
+        for(int i=0;i<n;i++){
+            cout<<b[i]<<" ";
+        }
+        cout<<endl;
+    }
+}
