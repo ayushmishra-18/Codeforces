e0a745560be88f8be5ea71dea80cb374cc25e413
Ayush Mishra <2006ayushmishra@gmail.com>
2026-06-07 17:28:27 +0530

    Add Lucky Division [A] from Codeforces

---
+++ 122/A - Lucky Division/solution.cpp
@@ -0,0 +1,31 @@
+//A. Lucky Division
+ 
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long int
+ 
+/*
+Khatarnak brute force
+*/
+ 
+void fast_io() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(NULL);
+    std::cout.tie(NULL);
+}
+ 
+signed main() {
+    fast_io();
+    int n;
+    cin>>n;
+    if(n%4==0||n%7==0||n%44==0||n%47==0||n%74==0||n%77==0||n%444==0||n%447==0||n%474==0||n%477==0||n%744==0||n%747==0||n%774==0||n%777==0){
+        cout<<"YES"<<endl;;
+        
+    }
+    else{
+        cout<<"NO"<<endl;;
+    }
+ 
+ 
+    return 0;
+}
