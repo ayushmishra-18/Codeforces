ece5f34f68319a062be9801e473ceb4c0fdb3302
Ayush Mishra <2006ayushmishra@gmail.com>
2026-06-12 15:58:46 +0530

    Add Queries about less or equal elements [B] from Codeforces

---
+++ 600/B - Queries about less or equal elements/solution.cpp
@@ -0,0 +1,53 @@
+//B. Queries about less or equal elements
+ 
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long int
+#define pb push_back
+#define all(v) v.begin(), v.end()
+#define endl '
+'
+ 
+typedef vector<int> vi;
+ 
+const int INF = 1e18;
+const int MOD = 1e9 + 7;
+ 
+/*
+Simple
+*/
+ 
+void fast_io() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+}
+ 
+void solve() {
+    int n,m;
+    cin >> n>>m;
+    vi arr(n),brr(m);
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    for (int i = 0; i < m; i++) {
+        cin >> brr[i];
+    }
+    sort(all(arr));
+    for(int j=0;j<m;j++){
+        int i=upper_bound(all(arr),brr[j])-arr.begin();
+        cout<<i<<" ";
+    }
+ 
+}
+ 
+signed main() {
+    fast_io();
+    int t = 1;
+    //cin >> t;
+    while(t--) {
+        solve();
+    }
+    
+    return 0;
+}
