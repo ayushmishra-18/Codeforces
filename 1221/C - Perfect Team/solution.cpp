010031519ab75e13f7cde0729b7434c1523c197e
Ayush Mishra <2006ayushmishra@gmail.com>
2026-06-13 08:34:34 +0530

    Add Perfect Team [C] from Codeforces

---
+++ 1221/C - Perfect Team/solution.cpp
@@ -0,0 +1,44 @@
+//C. Perfect Team
+ 
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long int
+#define pb push_back
+#define all(v) v.begin(), v.end()
+#define endl '
+'
+ 
+typedef vector<int> vi;
+ 
+const int INF = 1e18;
+const int MOD = 1e9 + 7;
+ 
+/*
+Simple Observation
+*/
+ 
+void fast_io() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+}
+ 
+void solve() {
+    int c,m,x;
+    cin >> c>>m>>x;
+    int sum= c+m+x;
+    int ans=min({c,m,sum/3});
+    cout<<ans<<endl;
+    
+}
+ 
+signed main() {
+    fast_io();
+    int t = 1;
+    cin >> t;
+    while(t--) {
+        solve();
+    }
+    
+    return 0;
+}
