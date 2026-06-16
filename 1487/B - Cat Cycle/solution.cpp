40d7d78931489cfff5ebb3531c00d0d9058416b7
Ayush Mishra <2006ayushmishra@gmail.com>
2026-06-16 07:29:37 +0530

    Add Cat Cycle [B] from Codeforces

---
+++ 1487/B - Cat Cycle/solution.cpp
@@ -0,0 +1,49 @@
+//B. Cat Cycle
+ 
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long int
+#define pb push_back
+#define all(v) v.begin(), v.end()
+#define endl '
+'
+ 
+typedef vector<int> vi;
+ 
+const int INF = 1e18;
+const int MOD = 1e9 + 7;
+ 
+/*
+ 
+*/
+ 
+void fast_io() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+}
+ 
+void solve() {
+    int n,k;
+    cin >> n>>k;
+    k--;
+    if(n%2==0){
+        cout<<(k%n)+1<<endl;
+    }
+    else{
+        cout<<(k+k/(n/2))%n+1<<endl;
+    }
+   
+    
+}
+ 
+signed main() {
+    fast_io();
+    int t = 1;
+    cin >> t;
+    while(t--) {
+        solve();
+    }
+    
+    return 0;
+}
