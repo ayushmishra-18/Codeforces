9d854ad9c4f5cb2aea74b463ee6b1c941bbb61c5
Ayush Mishra <2006ayushmishra@gmail.com>
2026-06-17 08:22:13 +0530

    Add Next Test [A] from Codeforces

---
+++ 27/A - Next Test/solution.cpp
@@ -0,0 +1,55 @@
+//A. Next Test
+ 
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long int
+#define pb push_back
+#define all(v) v.begin(), v.end()
+#define endl '
+'
+ 
+typedef vector<int> vi;
+ 
+const int INF = 1e18;
+const int MOD = 1e9 + 7;
+ 
+/*
+ 
+*/
+ 
+void fast_io() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+}
+ 
+void solve() {
+    int n;
+    cin >> n;
+    vi arr(n);
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    vi brr(3002,0);
+    for(int i=0;i<n;i++){
+        brr[arr[i]]++;
+    }
+    for(int i=1;i<=3001;i++){
+        if(brr[i]==0){
+            cout<<i<<endl;
+            return;
+        }
+    }
+ 
+}
+ 
+signed main() {
+    fast_io();
+    int t = 1;
+    //cin >> t;
+    while(t--) {
+        solve();
+    }
+    
+    return 0;
+}
