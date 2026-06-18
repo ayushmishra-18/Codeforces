3987e7263985ccf0678393ef205b8bab86730e79
Ayush Mishra <2006ayushmishra@gmail.com>
2026-06-18 07:28:24 +0530

    Add Balanced Team [C] from Codeforces

---
+++ 1133/C - Balanced Team/solution.cpp
@@ -0,0 +1,58 @@
+//C. Balanced Team
+ 
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long int
+#define double long double
+#define pb push_back
+#define all(v) v.begin(), v.end()
+#define endl '
+'
+ 
+typedef vector<int> vi;
+ 
+const int INF = 1e18;
+const int MOD = 1e9 + 7;
+ 
+/*
+ 
+*/
+ 
+void fast_io() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+}
+ 
+void solve() {
+    int n;
+    cin >> n;
+    vi arr(n);
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    sort(all(arr));
+    int ans=0;
+    int l=0;
+    int r=0;
+    while(l<n){
+        while(r<n && arr[r]-arr[l]<=5){
+            r++;
+            ans=max(ans,r-l);
+        }
+        l++;
+    }
+    cout<<ans<<endl;
+    
+}
+ 
+signed main() {
+    fast_io();
+    int t = 1;
+    //cin >> t;
+    while(t--) {
+        solve();
+    }
+    
+    return 0;
+}
