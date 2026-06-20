76b023142569e5a6824e7d4f9022d8879b67fb0c
Ayush Mishra <2006ayushmishra@gmail.com>
2026-06-20 16:40:25 +0530

    Add Yarik and Array [C] from Codeforces

--- 1899/C - Yarik and Array/solution.cpp
+++ 1899/C - Yarik and Array/solution.cpp
@@ -1,1 +1,31 @@
-//C. Yarik and Array #include<bits/stdc++.h>using namespace std;#define int long long intsigned main(){    int t;    cin>>t;    while(t--){        int n;        cin>>n;        vector<int>arr(n);        for(int i=0;i<n;i++){            cin>>arr[i];        }        int sum=0;        int ans=arr[0];        for(int i=0;i<n;i++){            if(i>0 && (abs(arr[i]%2)==abs(arr[i-1]%2))){                sum=0;            }            sum=sum+arr[i];            ans=max(ans,sum);            if(sum<0){                sum=0;            }                    }        cout<<ans<<endl;    }}
+//C. Yarik and Array
+ 
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long int
+signed main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        vector<int>arr(n);
+        for(int i=0;i<n;i++){
+            cin>>arr[i];
+        }
+        int sum=0;
+        int ans=arr[0];
+        for(int i=0;i<n;i++){
+            if(i>0 && (abs(arr[i]%2)==abs(arr[i-1]%2))){
+                sum=0;
+            }
+            sum=sum+arr[i];
+            ans=max(ans,sum);
+            if(sum<0){
+                sum=0;
+            }
+            
+        }
+        cout<<ans<<endl;
+    }
+}
