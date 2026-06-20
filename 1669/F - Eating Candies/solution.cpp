6b80f91af2dac279726202917a8d08972e49aa52
Ayush Mishra <2006ayushmishra@gmail.com>
2026-06-20 16:41:08 +0530

    Add Eating Candies [F] from Codeforces

--- 1669/F - Eating Candies/solution.cpp
+++ 1669/F - Eating Candies/solution.cpp
@@ -1,1 +1,35 @@
-//F. Eating Candies #include<bits/stdc++.h>using namespace std;#define int long long intsigned main(){    int t;    cin>>t;    while(t--){        int n;        cin>>n;        vector<int>arr(n);        for(int i=0;i<n;i++){            cin>>arr[i];        }            int l=0,r=0;        int i=0,j=n-1;        int count=0;        while(i<=j){            if(l<=r){                l=l+arr[i];                i++;            }            else{                r=r+arr[j];                j--;            }            if(l==r){                count=i+(n-1-j);            }        }        cout<<count<<endl;    }}
+//F. Eating Candies
+ 
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long int
+signed main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        vector<int>arr(n);
+        for(int i=0;i<n;i++){
+            cin>>arr[i];
+        }
+    
+        int l=0,r=0;
+        int i=0,j=n-1;
+        int count=0;
+        while(i<=j){
+            if(l<=r){
+                l=l+arr[i];
+                i++;
+            }
+            else{
+                r=r+arr[j];
+                j--;
+            }
+            if(l==r){
+                count=i+(n-1-j);
+            }
+        }
+        cout<<count<<endl;
+    }
+}
