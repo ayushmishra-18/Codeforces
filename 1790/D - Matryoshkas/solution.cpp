588e12e63f230d878d9239bc7669873404e796e6
Ayush Mishra <2006ayushmishra@gmail.com>
2026-06-20 16:42:27 +0530

    Add Matryoshkas [D] from Codeforces

--- 1790/D - Matryoshkas/solution.cpp
+++ 1790/D - Matryoshkas/solution.cpp
@@ -1,1 +1,45 @@
-//D. Matryoshkas #include <bits/stdc++.h>using namespace std;#define int long long int void fast_io() {    std::ios_base::sync_with_stdio(false);    std::cin.tie(NULL);    std::cout.tie(NULL);} signed main() {    fast_io();    int t;    cin>>t;    while(t--){        int n;        cin>>n;        vector<int>arr(n);        for(int i=0;i<n;i++){            cin>>arr[i];        }        sort(arr.begin(),arr.end());        int count=0;        int i=0,j=1;        while(j<n){            if(arr[i]+1==arr[j]){                count++;                i++,j++;            }            else if(arr[i]==arr[j]){                j++;            }            else{                i++;                if(i==j){                    j++;                }            }        }        cout<<n-count<<endl;    }    return 0;}
+//D. Matryoshkas
+ 
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long int
+ 
+void fast_io() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(NULL);
+    std::cout.tie(NULL);
+}
+ 
+signed main() {
+    fast_io();
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        vector<int>arr(n);
+        for(int i=0;i<n;i++){
+            cin>>arr[i];
+        }
+        sort(arr.begin(),arr.end());
+        int count=0;
+        int i=0,j=1;
+        while(j<n){
+            if(arr[i]+1==arr[j]){
+                count++;
+                i++,j++;
+            }
+            else if(arr[i]==arr[j]){
+                j++;
+            }
+            else{
+                i++;
+                if(i==j){
+                    j++;
+                }
+            }
+        }
+        cout<<n-count<<endl;
+    }
+    return 0;
+}
