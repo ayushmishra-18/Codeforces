fb659b9a7da9c9572e87148cd9b977124c5d6037
Ayush Mishra <2006ayushmishra@gmail.com>
2026-06-20 16:39:44 +0530

    Add Add and Divide [A] from Codeforces

--- 1485/A - Add and Divide/solution.cpp
+++ 1485/A - Add and Divide/solution.cpp
@@ -1,1 +1,28 @@
-//A. Add and Divide #include<bits/stdc++.h>using namespace std;#define int long long intsigned main(){    int t;    cin>>t;    while(t--){        int a,b;        cin>>a>>b;        int ans=99999999;        for(int i=0;i<=30;i++){            if(b+i==1){                continue;            }            int curr_a=a;            int curr_b=b+i;            int count=i;            while(curr_a>0){                curr_a/=curr_b;                count++;            }            ans=min(ans,count);        }        cout<<ans<<endl;    }}
+//A. Add and Divide
+ 
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long int
+signed main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int a,b;
+        cin>>a>>b;
+        int ans=99999999;
+        for(int i=0;i<=30;i++){
+            if(b+i==1){
+                continue;
+            }
+            int curr_a=a;
+            int curr_b=b+i;
+            int count=i;
+            while(curr_a>0){
+                curr_a/=curr_b;
+                count++;
+            }
+            ans=min(ans,count);
+        }
+        cout<<ans<<endl;
+    }
+}
