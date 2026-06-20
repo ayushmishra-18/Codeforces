dec78f353d9f51f4d9ecdccfbfe7bf5758046a97
Ayush Mishra <2006ayushmishra@gmail.com>
2026-06-20 17:07:00 +0530

    Add Destroying Towers [A] from Codeforces

---
+++ 2237/A - Destroying Towers/solution.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long int
+#define double long double
+#define pb push_back
+#define all(v) v.begin(), v.end()
+#define endl '
+'
+ 
+typedef vector<int> vi;
+ 
+const int INF = 1e18;
+const int MOD = 1e9 + 7;
+ 
+/*
+ 
+*/
+ 
+void fast_io() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+}
+ 
+void solve() {
+    int n;
+    cin >> n;
+    vi arr(n);
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    int ans=0;
+    int mini=INF;
+    for(int i=0;i<n;i++){
+        mini=min(mini,arr[i]);
+        ans+=mini;
+    }
+    cout<<ans<<endl;
+}
+ 
+signed main() {
+    fast_io();
+    int t = 1;
+    cin >> t;
+    while(t--) {
+        solve();
+    }
+    
+    return 0;
+}
