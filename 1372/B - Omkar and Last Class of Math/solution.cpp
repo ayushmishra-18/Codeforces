8be7898d019ec4f0ebf80d8006f67b69bd6f5f84
Ayush Mishra <2006ayushmishra@gmail.com>
2026-06-20 16:43:07 +0530

    Add Omkar and Last Class of Math [B] from Codeforces

--- 1372/B - Omkar and Last Class of Math/solution.cpp
+++ 1372/B - Omkar and Last Class of Math/solution.cpp
@@ -1,1 +1,47 @@
-//B. Omkar and Last Class of Math #include <bits/stdc++.h>using namespace std;#define int long long int void fast_io() {    std::ios_base::sync_with_stdio(false);    std::cin.tie(NULL);    std::cout.tie(NULL);} signed main() {    fast_io();    int t;    cin>>t;    while(t--){        int n;        cin>>n;        int a=0;        if(n%2==0){            cout<<n/2<<" "<<n/2<<endl;        }                else{            for(int i=2;i*i<=n;i++){                if(n%i==0){                    a=1;                    break;                }            }            if(a==0){                cout<<1<<" "<<n-1<<endl;            }            if(a==1){                int z=0;                for(int i=3;i*i<=n;i++){                    if(n%i==0){                        z=max(z,n/i);                    }                }                cout<<z<<" "<<n-z<<endl;;            }        }    }    return 0;}
+//B. Omkar and Last Class of Math
+ 
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long int
+ 
+void fast_io() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(NULL);
+    std::cout.tie(NULL);
+}
+ 
+signed main() {
+    fast_io();
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        int a=0;
+        if(n%2==0){
+            cout<<n/2<<" "<<n/2<<endl;
+        }
+        
+        else{
+            for(int i=2;i*i<=n;i++){
+                if(n%i==0){
+                    a=1;
+                    break;
+                }
+            }
+            if(a==0){
+                cout<<1<<" "<<n-1<<endl;
+            }
+            if(a==1){
+                int z=0;
+                for(int i=3;i*i<=n;i++){
+                    if(n%i==0){
+                        z=max(z,n/i);
+                    }
+                }
+                cout<<z<<" "<<n-z<<endl;;
+            }
+        }
+    }
+    return 0;
+}
