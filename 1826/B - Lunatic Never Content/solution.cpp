3152667480cc42ac03842efa607c1a21cf53d34e
Ayush Mishra <2006ayushmishra@gmail.com>
2026-06-20 16:41:01 +0530

    Add Lunatic Never Content [B] from Codeforces

--- 1826/B - Lunatic Never Content/solution.cpp
+++ 1826/B - Lunatic Never Content/solution.cpp
@@ -1,1 +1,21 @@
-#include<bits/stdc++.h>using namespace std;#define int long long intsigned main(){    int t;    cin>>t;    while(t--){        int n;        cin>>n;        vector<int>arr(n);        for(int i=0;i<n;i++){            cin>>arr[i];        }        int a=0;        for(int i=0;i<n;i++){            int b=abs(arr[i]-arr[n-i-1]);            a=__gcd(a,b);        }        cout<<a<<endl;    }}
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long int
+signed main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        vector<int>arr(n);
+        for(int i=0;i<n;i++){
+            cin>>arr[i];
+        }
+        int a=0;
+        for(int i=0;i<n;i++){
+            int b=abs(arr[i]-arr[n-i-1]);
+            a=__gcd(a,b);
+        }
+        cout<<a<<endl;
+    }
+}
