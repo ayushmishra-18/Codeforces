55604011fe93970fb780fc49cba5398f83ee9222
Ayush Mishra <2006ayushmishra@gmail.com>
2026-06-20 17:03:25 +0530

    Add Games on the Train [A] from Codeforces

---
+++ 2236/A - Games on the Train/solution.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long int
+#define pb push_back
+#define all(v) v.begin(), v.end()
+#define endl '
+'
+ 
+typedef vector<int> vi;
+ 
+const int INF = 1e18;
+const int MOD = 1e9 + 7;
+ 
+/*
+ 
+*/
+ 
+void fast_io() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+}
+ 
+void solve() {
+    int n;
+    cin >> n;
+    vi arr(n);
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    int maxi=*max_element(all(arr));
+    int mini=*min_element(all(arr));
+    cout<<maxi-mini+1<<endl;
+}
+ 
+signed main() {
+    fast_io();
+    int t = 1;
+    cin >> t;
+    while(t--) {
+        solve();
+    }
+    
+    return 0;
+}
