8117d4aaa70cd5dea705cea34a952d7716cbed65
Ayush Mishra <2006ayushmishra@gmail.com>
2026-06-20 16:35:07 +0530

    Add Two Permutations [A] from Codeforces

---
+++ 1761/A - Two Permutations/solution.cpp
@@ -0,0 +1,19 @@
+//A. Two Permutations
+ 
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long int 
+signed main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n,a,b;
+        cin>>n>>a>>b;
+        if(n-a-b>=2 || (((a==b) && (a==n))&& b==n)){
+            cout<<"Yes"<<endl;
+        }
+        else{
+            cout<<"No"<<endl;
+        }
+    }
+}
