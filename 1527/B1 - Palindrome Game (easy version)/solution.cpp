8ab18387291634bdf1b5609f78988791c36205fb
Ayush Mishra <2006ayushmishra@gmail.com>
2026-06-20 16:42:32 +0530

    Add Palindrome Game (easy version) [B1] from Codeforces

--- 1527/B1 - Palindrome Game (easy version)/solution.cpp
+++ 1527/B1 - Palindrome Game (easy version)/solution.cpp
@@ -1,1 +1,36 @@
-//B1. Palindrome Game (easy version) #include <bits/stdc++.h>using namespace std;#define int long long int void fast_io() {    std::ios_base::sync_with_stdio(false);    std::cin.tie(NULL);    std::cout.tie(NULL);} signed main() {    fast_io();    int t;    cin>>t;    while(t--){        int n;        cin>>n;        string s;        cin>>s;        int count=0;        for(int i=0;i<n;i++){            if(s[i]=='0'){                count++;            }        }        if(count==1 or count%2==0){            cout<<"BOB"<<endl;        }        else{            cout<<"ALICE"<<endl;        }    }    return 0;}
+//B1. Palindrome Game (easy version)
+ 
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long int
+ 
+void fast_io() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(NULL);
+    std::cout.tie(NULL);
+}
+ 
+signed main() {
+    fast_io();
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        string s;
+        cin>>s;
+        int count=0;
+        for(int i=0;i<n;i++){
+            if(s[i]=='0'){
+                count++;
+            }
+        }
+        if(count==1 or count%2==0){
+            cout<<"BOB"<<endl;
+        }
+        else{
+            cout<<"ALICE"<<endl;
+        }
+    }
+    return 0;
+}
