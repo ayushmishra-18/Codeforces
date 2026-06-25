057f1a10e6276785071b4c820a8ef1f5ada167f8
Ayush Mishra <2006ayushmishra@gmail.com>
2026-06-25 19:10:04 +0530

    Add Divisor Subtraction [B] from Codeforces

---
+++ 1076/B - Divisor Subtraction/solution.cpp
@@ -0,0 +1,50 @@
+//B. Divisor Subtraction
+ 
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long int
+#define double long double
+#define pb push_back
+#define all(v) v.begin(), v.end()
+#define endl '
+'
+ 
+typedef vector<int> vi;
+ 
+const int INF = 1e18;
+const int MOD = 1e9 + 7;
+ 
+/*
+ 
+*/
+ 
+void fast_io() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+}
+ 
+void solve() {
+    int n;
+    cin >> n;
+    int d=n;
+    for(int i=2;i*i<=n;i++){
+        if(n%i==0){
+            d=i; 
+            break;
+        }
+    }
+    
+    cout<<1+(n-d)/2<<endl;
+}
+ 
+signed main() {
+    fast_io();
+    int t = 1;
+    //cin >> t;
+    while(t--) {
+        solve();
+    }
+    
+    return 0;
+}
