c5ad454b1e9516929d0388b32bac61bf54362dfb
Ayush Mishra <2006ayushmishra@gmail.com>
2026-07-06 18:26:17 +0530

    Add Divide and Conquer [A] from Codeforces

---
+++ 2241/A - Divide and Conquer/solution.cpp
@@ -0,0 +1,55 @@
+//A. Divide and Conquer
+ 
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long int
+#define double long double
+#define pb push_back
+#define all(v) v.begin(), v.end()
+#define rall(a) a.rbegin(),a.rend()
+#define gcd(a,b) __gcd(a,b)
+#define endl '
+'
+ 
+typedef vector<int> vi;
+ 
+const int INF = 1e18;
+const int MOD = 1e9 + 7;
+ 
+/*
+ 
+*/
+ 
+void fast_io() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+}
+ 
+void solve() {
+    int x,y;
+    cin>>x>>y;
+    if(x<y){
+        cout<<"NO"<<endl;
+        return;
+    }
+    if(x%y==0){
+        cout<<"YES"<<endl;
+        return;
+    }
+    cout<<"NO"<<endl;
+    
+ 
+    
+}
+ 
+signed main() {
+    fast_io();
+    int tc = 1;
+    cin >> tc;
+    while(tc--) {
+        solve();
+    }
+    
+    return 0;
+}
