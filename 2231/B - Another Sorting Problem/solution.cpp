58bf9fd059ec99cb8def11deaa85c77f12649afe
Ayush Mishra <2006ayushmishra@gmail.com>
2026-07-07 18:24:52 +0530

    Add Another Sorting Problem [B] from Codeforces

---
+++ 2231/B - Another Sorting Problem/solution.cpp
@@ -0,0 +1,64 @@
+//B. Another Sorting Problem
+ 
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long int
+#define double long double
+#define pb push_back
+#define all(v) v.begin(), v.end()
+#define rall(a) a.rbegin(),a.rend()
+#define gcd(a,b) __gcd(a,b)
+#define endl '
+'
+ 
+typedef vector<int> vi;
+ 
+const int INF = 1e18;
+const int MOD = 1e9 + 7;
+ 
+/*
+ 
+*/
+ 
+void fast_io() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+}
+ 
+void solve() {
+    int n;
+    cin >> n;
+    vi arr(n);
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+        
+    }
+    int k=0;
+    for(int i=0;i<n-1;i++){
+        k=max(k,arr[i]-arr[i+1]);
+    }
+    for(int i=0;i<n-1;i++){
+        if(arr[i]>arr[i+1]){
+            arr[i+1]=arr[i+1]+k;
+        }
+    }
+    if(is_sorted(all(arr))){
+        cout<<"YES"<<endl;
+        return;
+    }
+    
+    cout<<"NO"<<endl;
+    
+}
+ 
+signed main() {
+    fast_io();
+    int tc = 1;
+    cin >> tc;
+    while(tc--) {
+        solve();
+    }
+    
+    return 0;
+}
