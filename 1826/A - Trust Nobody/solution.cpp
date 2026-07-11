66b013914a0ebd22461979ba810bd342910c8d4d
Ayush Mishra <2006ayushmishra@gmail.com>
2026-07-11 16:10:57 +0530

    Add Trust Nobody [A] from Codeforces

---
+++ 1826/A - Trust Nobody/solution.cpp
@@ -0,0 +1,60 @@
+//A. Trust Nobody
+ 
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long int
+#define double long double
+#define pb push_back
+#define all(v) v.begin(), v.end()
+#define rall(a) a.rbegin(),a.rend()
+#define gcd(a,b) __gcd(a,b)
+#define endl '
+'
+ 
+typedef vector<int> vi;
+ 
+const int INF = 1e18;
+const int MOD = 1e9 + 7;
+ 
+/*
+ 
+*/
+ 
+void fast_io() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+}
+ 
+void solve() {
+    int n;
+    cin >> n;
+    vi arr(n);
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    for(int i=0;i<n;i++){
+        int a=0;
+        for(int j=0;j<n;j++){
+            if(arr[j]>i){
+                a++;
+            }
+        }
+        if(a==i){
+            cout<<i<<endl;
+            return;
+        }
+    }
+    cout<<-1<<endl;
+}
+ 
+signed main() {
+    fast_io();
+    int tc = 1;
+    cin >> tc;
+    while(tc--) {
+        solve();
+    }
+    
+    return 0;
+}
