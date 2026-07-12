6f55851e57ead9572e0bd3af5c2d6726af6e710b
Ayush Mishra <2006ayushmishra@gmail.com>
2026-07-12 22:54:19 +0530

    Add Brightness Begins [B] from Codeforces

---
+++ 2020/B - Brightness Begins/solution.cpp
@@ -0,0 +1,46 @@
+//B. Brightness Begins
+ 
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long int
+#define double long double
+#define pb push_back
+#define all(v) v.begin(), v.end()
+#define rall(a) a.rbegin(),a.rend()
+#define gcd(a,b) __gcd(a,b)
+#define endl '
+'
+ 
+typedef vector<int> vi;
+ 
+const int INF = 1e18;
+const int MOD = 1e9 + 7;
+ 
+/*
+k+sqrt(k)+0.5
+Editorial
+*/
+ 
+void fast_io() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+}
+ 
+void solve() {
+    int k;
+    cin >> k;
+    cout<<k+(int)(sqrtl(k)+0.5)<<endl;
+    
+}
+ 
+signed main() {
+    fast_io();
+    int tc = 1;
+    cin >> tc;
+    while(tc--) {
+        solve();
+    }
+    
+    return 0;
+}
