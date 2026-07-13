d170b1c66b2234a5863a57b1046f0723c3b982e8
Ayush Mishra <2006ayushmishra@gmail.com>
2026-07-13 22:10:42 +0530

    Add farmpiggie and Subset Sum [A] from Codeforces

---
+++ 2246/A - farmpiggie and Subset Sum/solution.cpp
@@ -0,0 +1,48 @@
+//A. farmpiggie and Subset Sum
+ 
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long int
+#define double long double
+#define pb push_back
+#define all(v) v.begin(), v.end()
+#define rall(a) a.rbegin(),a.rend()
+#define gcd(a,b) __gcd(a,b)
+#define endl '
+'
+ 
+typedef vector<int> vi;
+ 
+const int INF = 1e18;
+const int MOD = 1e9 + 7;
+ 
+/*
+2 1 4 3 6 5 ....
+-> 2 2 12 12 30 30
+*/
+ 
+void fast_io() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+}
+ 
+void solve() {
+    int n;
+    cin >> n;
+    for(int i=1;i<n;i=i+2){
+        cout<<i+1<<" "<<i<<" ";
+    }
+    cout<<endl;
+}
+ 
+signed main() {
+    fast_io();
+    int tc = 1;
+    cin >> tc;
+    while(tc--) {
+        solve();
+    }
+    
+    return 0;
+}
