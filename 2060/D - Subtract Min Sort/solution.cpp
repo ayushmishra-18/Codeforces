94e54c3373d6dd6a574b59d2f9d52864784f86b8
Ayush Mishra <2006ayushmishra@gmail.com>
2026-07-15 21:09:03 +0530

    Add Subtract Min Sort [D] from Codeforces

---
+++ 2060/D - Subtract Min Sort/solution.cpp
@@ -0,0 +1,60 @@
+//D. Subtract Min Sort
+ 
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long int
+#define double long double
+#define pb push_back
+#define all(v) v.begin(), v.end()
+#define rall(a) a.rbegin(),a.rend()
+#define gcd(a,b) __gcd(a,b)
+#define endl '
+'
+ 
+typedef vector<int> vi;
+ 
+const int INF = 1e18;
+const int MOD = 1e9 + 7;
+ 
+/*
+ 
+*/
+ 
+void fast_io() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+}
+ 
+void solve() {
+    int n;
+    cin >> n;
+    vi arr(n);
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    for(int i=0;i<n-1;i++){
+        int a=min(arr[i],arr[i+1]);
+        arr[i]-=a;
+        arr[i+1]-=a;
+ 
+    }
+    for(int i=0;i<n-1;i++){
+        if(arr[i]>arr[i+1]){
+            cout<<"NO"<<endl;
+            return;
+        }
+    }
+    cout<<"YES"<<endl;
+}
+ 
+signed main() {
+    fast_io();
+    int tc = 1;
+    cin >> tc;
+    while(tc--) {
+        solve();
+    }
+    
+    return 0;
+}
